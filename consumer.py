9dacd8137404a09624c519cbafea705ebb64f45a
Varun Bharadwaj <varunbwaj@gmail.com>
2024-04-21 16:06:33 +0530

    Spark Streaming part done
    Three topics each of different subreddits are streamed in aggregation on subreddit scores are done and stored in a table and the raw data is stored in another table

--- consumer.py
+++ consumer.py
@@ -1,23 +1,3 @@
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import *
-# from pyspark.sql.types import *
-
-# # Spark Session
-# spark = SparkSession.builder.appName("RedditStreamingApp").getOrCreate()
-
-# # Define schema
-# schema = StructType([StructField("text", StringType(), True),
-#                      StructField("subreddit", StringType(), True),
-#                      StructField("score", IntegerType(), True),
-#                      StructField("num_comments", IntegerType(), True)])
-
-# # Read data from Kafka
-# reddit_df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "reddit_tweets").load()
-# reddit_df = reddit_df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
-
-# # Write data to console
-# query = reddit_df.writeStream.outputMode("update").format("console").start()
-# query.awaitTermination()
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
@@ -38,9 +18,28 @@ reddit_df_1 = reddit_df_1.select(from_json(col("value").cast("string"), schema).
 reddit_df_2 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "reddit_tweets_2").load()
 reddit_df_2 = reddit_df_2.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
 
-# Write data to console
-query1 = reddit_df_1.writeStream.outputMode("update").format("console").start()
-query2 = reddit_df_2.writeStream.outputMode("update").format("console").start()
+reddit_df_3 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "reddit_tweets_3").load()
+reddit_df_3 = reddit_df_3.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
+
+# Combine all dataframes
+combined_df = reddit_df_1.union(reddit_df_2).union(reddit_df_3)
+
+# Perform aggregation
+aggregated_df = combined_df.groupBy("subreddit").agg(avg("score").alias("average_score"))
+
+def write_to_mysql(df, epoch_id, table_name):
+    df.write.format('jdbc').options(
+          url='jdbc:mysql://localhost:3306/dbtproj',
+          driver='com.mysql.jdbc.Driver',
+          dbtable=table_name,
+          user='root',
+          password='varunbwaj').mode('append').save()
+
+# Write raw data to MySQL
+query1 = combined_df.writeStream.outputMode("append").foreachBatch(lambda df, epoch_id: write_to_mysql(df, epoch_id, 'reddit_data')).start()
+
+# Write aggregated data to MySQL
+query2 = aggregated_df.writeStream.outputMode("complete").foreachBatch(lambda df, epoch_id: write_to_mysql(df, epoch_id, 'reddit_avg_score')).start()
 
 query1.awaitTermination()
 query2.awaitTermination()
--- producer.py
+++ producer.py
@@ -1,23 +1,3 @@
-# import praw
-# from kafka import KafkaProducer
-# import json
-# import time
-
-# # Reddit API credentials
-# reddit = praw.Reddit(client_id='7rae84HhPjnd9P1382BH9A',
-#                      client_secret='bXdbWNh__ibW6CmVvsSgbnynUk_h-w',
-#                      user_agent='dbtAssignmentv1')
-
-# # Kafka Producer
-# producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-#                          value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-
-# # Fetch data from Reddit and send it to Kafka
-# while True:
-#     for submission in reddit.subreddit('formula1').hot(limit=10):
-#         tweet_data = {"text": submission.title, "subreddit": str(submission.subreddit), "score": submission.score, "num_comments": submission.num_comments}
-#         producer.send('reddit_tweets', value=tweet_data)
-#     time.sleep(5)  
 import praw
 from kafka import KafkaProducer
 import json
@@ -34,8 +14,8 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
 
 # Fetch data from Reddit and send it to Kafka
 while True:
-    for submission in reddit.subreddit('formula1').hot(limit=10):
-        tweet_data = {"text": submission.title, "subreddit": str(submission.subreddit), "score": submission.score, "num_comments": submission.num_comments}
-        producer.send('reddit_tweets_1', value=tweet_data)
-        producer.send('reddit_tweets_2', value=tweet_data)
+    for subreddit, topic in [('formula1', 'reddit_tweets_1'), ('AskReddit', 'reddit_tweets_2'), ('worldnews', 'reddit_tweets_3')]:
+        for submission in reddit.subreddit(subreddit).hot(limit=10):
+            tweet_data = {"text": submission.title, "subreddit": str(submission.subreddit), "score": submission.score, "num_comments": submission.num_comments}
+            producer.send(topic, value=tweet_data)
     time.sleep(5)
